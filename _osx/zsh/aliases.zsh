136bb12de775fa2eec6978932911b179e0158143
Mitchell Setsma <38572013+msetsma@users.noreply.github.com>
2026-02-12 09:26:44 -0500

    Clean up and modifications (mac focused).

--- _osx/zsh/aliases.zsh
+++ _osx/zsh/aliases.zsh
@@ -3,9 +3,6 @@ alias c='clear'
 alias home='cd $home'
 alias dotfiles='cd ~/.dotfiles'
 
-alias py='python3'
-alias python='python3'
-
 # EZA - better ls
 alias ls='eza -1ah --git-repos-no-status --icons'
 alias lt1='eza --icons --tree --level=1 --group-directories-first -h --long --no-permissions --no-user --no-time'
@@ -13,3 +10,14 @@ alias lt2='eza --icons --tree --level=2 --group-directories-first -h --long --no
 alias lt3='eza --icons --tree --level=3 --group-directories-first -h --long --no-permissions --no-user --no-time'
 alias lta='eza --icons --tree --group-directories-first -h --long --no-permissions --no-user --no-time'
 alias ltcopy='eza --tree | pbcopy'
+
+# git
+alias gfc='git_fzf_checkout'
+
+# python
+alias py='python3'
+alias python='python3'
+alias venv='manage_project_venv'
+
+# databricks
+alias db='databricks'
